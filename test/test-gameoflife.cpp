e68f27c726ee31b7775d540a961ac1c85967340c
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-23 15:10:46 +0200

    integrera-catch.h (added catch files)

---
+++ test/test-gameoflife.cpp
@@ -0,0 +1,10 @@
+/**
+* @file
+ * @author Elin Fritiofsson
+ * @date 23 october 2018
+ * @version 0.1
+ * @brief Contains all our tests.
+*/
+
+#include "catch.hpp"
+//#include "../include/"
---
+++ test/test-main.cpp
@@ -0,0 +1,10 @@
+/**
+* @file
+ * @author Elin Fritiofsson
+ * @date 23 october 2018
+ * @version 0.1
+ * @brief Defines CATCH_CONFIG_MAIN separately to shorten compile time.
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
