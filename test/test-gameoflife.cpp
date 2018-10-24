af80e15556290ab70bbb237b811e365a9bc049cf
Fredrik Arhusiander <frar1700@student.miun.se>
2018-10-24 11:27:39 +0200

    finished tests for different scenarios for constructor of cell.h

--- test/test-gameoflife.cpp
+++ test/test-gameoflife.cpp
@@ -3,7 +3,7 @@
  * @author Elin Fritiofsson
  * @date 23 october 2018
  * @version 0.1
- * @brief Contains all our tests.
+ * @brief Contains all the tests for the Cell class constructor
 */
 
 #include <catch.hpp>
@@ -11,32 +11,63 @@
 #include "../include/Cell_Culture/Cell.h"
 
 SCENARIO("We create a Cell object"){
-    GIVEN("a default Cell object"){
+    GIVEN("A default Cell object"){
         Cell cell;
-        THEN("it should be a non-rim cell and have action set to nothing"){
+        THEN("it should be a non-rim cell"){
             REQUIRE_FALSE(cell.isRimCell());
-            REQUIRE(cell.getNextGenerationAction() == DO_NOTHING);
         }
-        THEN("As default CellDetails are also set: It should have a dead color, age 0 and screen character #"){
+        THEN("The member color should be STATE_COLORS.DEAD") {
             REQUIRE(cell.getColor() == STATE_COLORS.DEAD);
+        }
+        THEN("The age should be 0") {
             REQUIRE(cell.getAge() == 0);
+        }
+        // The following two only need testing once since they are initially the same
+        // for all Cell objects, and can only change after initialization
+        THEN("The screen character should be #"){
             REQUIRE(cell.getCellValue() == '#');
         }
-
-    }
-
-    GIVEN("a cell object with action = GIVE_CELL_LIFE"){
-        Cell cell(false, GIVE_CELL_LIFE);
-        THEN("it should be a non-rim cell and have action reset to DO_NOTHING after object creation"){
-            REQUIRE_FALSE(cell.isRimCell());
+        THEN("It should have next action set to DO_NOTHING") {
             REQUIRE(cell.getNextGenerationAction() == DO_NOTHING);
         }
-        THEN("As default CellDetails are also set: It should have a living color, age 1 and screen character #"){
+    }
+
+    GIVEN("A cell object with action = GIVE_CELL_LIFE"){
+        Cell cell(false, GIVE_CELL_LIFE);
+        THEN("it should be a non-rim cell"){
+            REQUIRE_FALSE(cell.isRimCell());
+        }
+        THEN("The member color should be STATE_COLORS.LIVING") {
             REQUIRE(cell.getColor() == STATE_COLORS.LIVING);
+        }
+        THEN("The age should be 1") {
             REQUIRE(cell.getAge() == 1);
-            REQUIRE(cell.getCellValue() == '#');
         }
     }
 
+    GIVEN("A Cell object with action = IGNORE_CELL") {
+        Cell cell(false, IGNORE_CELL);
+        THEN("it should be a non-rim cell"){
+            REQUIRE_FALSE(cell.isRimCell());
+        }
+        THEN("The member color should be STATE_COLORS.DEAD") {
+            REQUIRE(cell.getColor() == STATE_COLORS.DEAD);
+        }
+        THEN("The age should be 0") {
+            REQUIRE(cell.getAge() == 0);
+        }
+    }
 
+    GIVEN("A rim cell") {
+        Cell cell(true);
+        THEN("it should be a rim cell"){
+            REQUIRE(cell.isRimCell());
+        }
+        THEN("The member color should be STATE_COLORS.DEAD") {
+            REQUIRE(cell.getColor() == STATE_COLORS.DEAD);
+        }
+        THEN("The age should be 0") {
+            REQUIRE(cell.getAge() == 0);
+        }
+    }
 }
