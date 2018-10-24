21af26a5e47052819a89f44c3cc9d7612e532c1f
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-24 09:27:48 +0200

    created a test scenario with two types of cell objects.

--- test/test-gameoflife.cpp
+++ test/test-gameoflife.cpp
@@ -7,4 +7,36 @@
 */
 
 #include <catch.hpp>
-//#include "../include/"
+#include <iostream>
+#include "../include/Cell_Culture/Cell.h"
+
+SCENARIO("We create a Cell object"){
+    GIVEN("a default Cell object"){
+        Cell cell;
+        THEN("it should be a non-rim cell and have action set to nothing"){
+            REQUIRE_FALSE(cell.isRimCell());
+            REQUIRE(cell.getNextGenerationAction() == DO_NOTHING);
+        }
+        THEN("As default CellDetails are also set: It should have a dead color, age 0 and screen character #"){
+            REQUIRE(cell.getColor() == STATE_COLORS.DEAD);
+            REQUIRE(cell.getAge() == 0);
+            REQUIRE(cell.getCellValue() == '#');
+        }
+
+    }
+
+    GIVEN("a cell object with action = GIVE_CELL_LIFE"){
+        Cell cell(false, GIVE_CELL_LIFE);
+        THEN("it should be a non-rim cell and have action reset to DO_NOTHING after object creation"){
+            REQUIRE_FALSE(cell.isRimCell());
+            REQUIRE(cell.getNextGenerationAction() == DO_NOTHING);
+        }
+        THEN("As default CellDetails are also set: It should have a living color, age 1 and screen character #"){
+            REQUIRE(cell.getColor() == STATE_COLORS.LIVING);
+            REQUIRE(cell.getAge() == 1);
+            REQUIRE(cell.getCellValue() == '#');
+        }
+    }
+
+
+}
