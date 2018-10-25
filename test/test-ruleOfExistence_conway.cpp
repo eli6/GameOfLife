162ca2fe362662a7a7fa032d3c07380c910c9813
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-25 17:49:20 +0200

    testa-ruleofexistence_conway.h (added scenario stubs)

---
+++ test/test-ruleOfExistence_conway.cpp
@@ -0,0 +1,28 @@
+/**
+* @file Cell.h
+ * @author
+ * @author Elin Fritiofsson
+ * @author Fredrik Arhusiander
+ * @date 23 october 2018
+ * @version 0.1
+ * @brief Contains all the tests for the Cell class constructor
+*/
+
+#include <catch.hpp>
+#include <iostream>
+#include "../include/GoL_Rules/RuleOfExistence_Conway.h"
+
+
+
+
+/***********************************
+* TESTING CONSTRUCTOR WITH DIFFERENT PARAMETERS
+***********************************/
+SCENARIO("Testing constructor"){
+
+}
+
+
+SCENARIO("Testing executeRule()"){
+
+}
