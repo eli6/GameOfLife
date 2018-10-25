86c0c9ab18e4cd08d51ae5ad484774aba91a5313
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-25 17:33:51 +0200

    testa-cell.h (added up-to-date version of ...vonNeumann.h to this branch and CMakeLists so branch can be run)

---
+++ include/GoL_Rules/RuleOfExistence_VonNeumann.h
@@ -0,0 +1,55 @@
+/**
+ * @file    RuleOfExistence_VonNeumann.h
+ * @author      Erik StrÃ¶m
+ * @date        October 2017
+ * @version     0.1
+ * @brief   Contains class RuleOfExistence_VonNeumann
+*/
+
+#ifndef GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
+#define GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
+
+#include "RuleOfExistence.h"
+/**
+*   @brief Von Neumann's RuleOfExistence, differs from Conway in that only 4 neighbours are accounted for.
+*
+*   @details Concrete Rule of existence, implementing Von Neumann's rule.
+*   Only difference from Conway is that neighbours are determined using only cardinal directions (N, E, S, W).
+*
+*   @test Test constructor/destructor and executeRule().
+*/
+class RuleOfExistence_VonNeumann : public RuleOfExistence
+{
+private:
+
+public:
+    /**
+     * @brief Constructor for a vonNeumann-Rule of Existence.
+     * @details Very similar to Conwways rule, except that only cardinal directions are used.
+     * PopulationLimits are set as:
+     * - UNDERPOPULATION	< 2
+     * - OVERPOPULATION   > 3
+     * - RESURRECTION     = 3
+     *
+     * @param cells Map of cells and their points.
+     */
+    RuleOfExistence_VonNeumann(map<Point, Cell>& cells)
+            : RuleOfExistence({ 2,3,3 }, cells, CARDINAL, "von_neumann") {}
+
+    /**
+    * @brief Class destructor.
+    * @details Default destructor for class.
+    * @test Check that it is run at end of scope.
+    */
+    ~RuleOfExistence_VonNeumann() {}
+
+    /**
+     * @brief Executes the rule to determine generational changes in the cell population.
+     * @details Calculates how many alive neighbors a (non-rim) cell has and determines what action to take
+       depending on neighbors and if the cell is alive or not. The cells new color will be set (for example dead/living) and a cell method is invoked so that the cell can take further action.
+     * @test Test that this method sets correct cell colors for different neighbor scenarios.
+     */
+    void executeRule();
+};
+
+#endif //GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
