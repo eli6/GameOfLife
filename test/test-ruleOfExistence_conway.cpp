492d9fa5c394cf59103034945e32026a4ffe45aa
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-26 18:02:23 +0200

    testa-ruleofexistence_conway.h (added test of number of living neighbours)

--- test/test-ruleOfExistence_conway.cpp
+++ test/test-ruleOfExistence_conway.cpp
@@ -13,8 +13,6 @@
 #include "../include/GoL_Rules/RuleOfExistence_Conway.h"
 
 
-
-
 /***********************************
 * TESTING CONSTRUCTOR WITH DIFFERENT PARAMETERS
 ***********************************/
@@ -29,36 +27,76 @@ SCENARIO("Testing executeRule()"){
         //We declare the objects needed for testing
         // we need 8 points to create the 8 neighbors
 
-        Cell aliveCell1;
-        Cell aliveCell2;
-        Cell aliveCell3;
-        Cell rimCell;
         map<Point, Cell> cells;
 
+        int living = 0;
 
+        //we create 8 scenarios with j+1 number of living cells.
+        for(int j=0; j < 8; j++) {
+            //count how many living cells we produce since we want to test 1-8 living cells in our matrix
+            int counter = 0;
+            //we generate a small 5x5 cell population to test executeRule()
+            for (int row = 0; row <= 4; row++) {
+                for (int column = 0; column <= 4; column++) {
 
-        //we generate a small 4x4 cell population to test executeRule()
-        for (int row = 0; row <= 3; row++) {
+                    // if cell is a rim cell
+                    if (column == 0 || row == 0 || column == 4 || row == 4) {
+                        cells[Point{column, row}] = Cell(true); // create a cell with rimCell state set to true
+                    } else if(counter <= j){ // If cell is an ordinary cell
+                        cells[Point{column, row}] = Cell(false, GIVE_CELL_LIFE); // create a ordinary living cell
 
-            for (int column = 0; column <= 3; column++) {
-                // if cell is a rim cell
-                if (column == 0 || row == 0 || column == 3 || row == 3) {
+                        //increment the counter for number of living cells we have in this run (we want to stop at j living cells).
+                        counter++;
 
-                    cells[Point{column, row}] = Cell(true); // create a cell with rimCell state set to true
+                    } else {
+                        cells[Point{column, row}] = Cell(false, KILL_CELL);
+                    }
                 }
-                else { // If cell is an ordinary cell
-                    //all cells except rim cells are alive.
-                    cells[Point{column, row}] = Cell(false, GIVE_CELL_LIFE); // create a ordinary living cell
 
-                }
+
+
             }
+
+            auto *conway = new RuleOfExistence_Conway(cells);
+
+
+            for (auto it=cells.begin(); it!=cells.end(); ++it) {
+                int aliveNeighbors = conway->countAlive(it->first);
+                Cell cell;
+                cell = conway->getCellAtPosition(it->first);
+
+                //now we dont want any rim cells to be alive
+                if(cell.isRimCell()){
+                    REQUIRE_FALSE(cell.isAlive());
+                }
+
+                //for the other cells, only the cells with 3 neigbours should be alive.
+                if (!cell.isRimCell()) {
+                    if (aliveNeighbors < 2) {
+                        cell.setNextGenerationAction(KILL_CELL);
+                        cell.updateState();
+                        REQUIRE_FALSE(cell.isAlive());
+                    }
+
+                    if (aliveNeighbors > 3) {
+                        cell.setNextGenerationAction(KILL_CELL);
+                        cell.updateState();
+                        REQUIRE_FALSE(cell.isAlive());
+                    }
+
+                    if (aliveNeighbors == 3) {
+                        cell.setNextGenerationAction(GIVE_CELL_LIFE);
+                        cell.updateState();
+                        REQUIRE(cell.isAlive());
+                    }
+                }
+
+            }
+
+            }
+
         }
-
-        auto *conway = new RuleOfExistence_Conway(cells);
-        conway->executeRule();
-
-        //each cell should have 3 alive neighbors:
 
     }
 
-}
+
