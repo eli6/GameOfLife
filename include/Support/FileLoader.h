9c5cac651fd5641595040d74c4e6321f65a05f75
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-30 14:09:10 +0100

    added @bug tags as needed

--- include/Support/FileLoader.h
+++ include/Support/FileLoader.h
@@ -10,7 +10,7 @@
 #define FileLoaderH
 
 #include <map>
-#include "Cell_Culture/Cell.h"
+#include "../Cell_Culture/Cell.h"
 #include "Globals.h"
 
 using namespace std;
@@ -22,6 +22,7 @@ using namespace std;
  * @test So that the member functions work as they should
 */
 class FileLoader {
+private:
 
 public:
     /**
@@ -37,7 +38,11 @@ public:
      * @param cells A reference to an object of the type map<Point, Cell>
      * @test 1) That the file is opened when a correct filename is used 2) That an exception
      * is thrown when not 3) That the correct world size is read 4) That a population of cells is created.
-     */
+     *  @bug Uncaught exception when using an incorrect seed file.
+     *  @bug Creates a cell population that is 2 cells too wide and 2 cells too high. Cell at point 19,9 is not a rim cell as expected. Change in if clause so that values between 0 and WORLD_DIMENSIONS.HEIGHT/WIDTH - 1 instead of +1 is used.
+     *  @bug Row 0 is also read from file although it contains rim cells, since that line isn't explicitly removed or skipped. It also reads a total of 10 rows starting from 0, and not 9 whiuch was expected, since WORLD_DIMENSIONS.HEIGHT+1 instead of -1 is used.
+     *  @bug " char cellState = iss.get();" gets the 0 at the start of the row and creates a dead cell, but that is a rimCell and should be skipped
+     * */
     void loadPopulationFromFile(map<Point, Cell>& cells);
 
 };
