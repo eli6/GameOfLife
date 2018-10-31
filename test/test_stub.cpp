80e872917d57be563beca43079ca61daa6cad567
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-31 17:32:35 +0100

    corrected code after review

--- test/test_stub.cpp
+++ test/test_stub.cpp
@@ -39,35 +39,46 @@ SCENARIO("testing loadPopulationFromFile"){
 
                     REQUIRE(cells[{0, 9}].isRimCell());
                 }
-                //This also fails
+                //This fails
                 THEN("CELL 19,9 should also be a rim cell (lower right corner)"){
                     REQUIRE(cells[{19, 9}].isRimCell());
                 }
 
                 THEN("We should be able to loop through the second row of cells and it should correspond to line 1 in our Population_Seed.txt"){
 
+                    // we passed in this row as seed for the second row.
+                    //00000010000011001100
+                    //this means that cells with x value 6, 12, 13, 16, 17 should have had nextAction set to GIVE_CELL_LIFE. Others to IGNORE_CELL.
+                    vector<int> v = {6, 12, 13, 16, 17};
+
                     for (auto it = cells.begin(); it != cells.end(); ++it){
                         //if the y(row) value is 1 and x (column) is between 0 and 19, we're on line 2.
                         if(it->first.y == 1 && (it->first.x >= 0 && it->first.x < 19)){
-                            // we passed in this row as seed for the second row.
-                            //00000010000011001100
-                            //this means that cells with x value 6, 12, 13, 16, 17 should have had nextAction set to GIVE_CELL_LIFE. Others to IGNORE_CELL.
-                            vector<int> v = {7, 12, 13, 16, 17};
-                            if(it->second.getColor() == STATE_COLORS.LIVING){
-                                int xValue = it->first.x;
-                                auto it = std::find(v.begin(), v.end(), xValue);
 
-                                //test fails
-                                //require returns 7 and not 6 if we correct the world dimensinos in fileLoader.cpp meaning the rim cell at the start of the row is mistakenly taken for a normal cell.
-                                //if we use the incorrect world dimension calculations dimensions +1, nothing is correct since a row with only zeroes (rim cells) is incorrectly used for line 2.
-                                REQUIRE(it!=v.end());
+                            //we store the x value for simplicity
+                            int xValue = it->first.x;
+                            vector<int>::iterator p;
+
+                            //we see if we can find that x value in our vector above.
+                            p = std::find(v.begin(), v.end(), xValue);
+
+                            //if we are on one of the cells in the vector v (that should be living)
+                            if(p != v.end()){
+                                //test below fails
+                                //if we put first.y == 2 in the if clause above, we get living cells on position 7, 13, 14, etc.. theyr shifted one to the right meaning the rim cell at the left is counted as a normal cell.
+                                //in this test: a row with only zeroes (rim cells) is incorrectly used for line 2.
+
+                                //it should have color living.
+                                REQUIRE(it->second.getColor() == STATE_COLORS.LIVING);
+                            }
+
+
                             }
                         }
                     }
                 }
             }
-            //this test fails because no exception is thrown, so I added a @bug.
-            AND_WHEN("we call file loader with a valid name but the file contains erroneous content"){
+           AND_WHEN("we call file loader with a valid name but the file contains erroneous content"){
                fileName = "../Population_Seed_With_Errors.txt";
                 REQUIRE_THROWS(fileLoader.loadPopulationFromFile(cells));
             }
@@ -80,4 +91,3 @@ SCENARIO("testing loadPopulationFromFile"){
 
     }
 
-}
