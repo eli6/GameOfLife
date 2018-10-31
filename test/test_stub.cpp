49f6cfabd66d0783184cc3ccc85007a22f2c970f
Elin Fritiofsson <elfr1702@student.miun.se>
2018-10-31 12:49:22 +0100

    added tests of different rule combinations

--- test/test_stub.cpp
+++ test/test_stub.cpp
@@ -26,11 +26,10 @@ SCENARIO("testing runParser"){
             char arg9[] = {"-or"};
             char arg10[] = {"erik"};
 
-            char *argv[] = {&arg0[0], &arg1[0], &arg2[0], &arg3[0], &arg4[0], &arg5[0], &arg6[0], &arg7[0], &arg8[0], &arg9[0], &arg10[0], NULL};
+            char *argv[] = {&arg0[0], &arg1[0], &arg2[0], &arg3[0], &arg4[0], &arg5[0], &arg6[0], &arg7[0], &arg8[0], &arg9[0], &arg10[0],
+                            nullptr};
 
-            std::cout << *arg6 << std::endl;
-
-            mParser.runParser(argv, 11);
+            ApplicationValues appValues = mParser.runParser(argv, 11);
 
             THEN("filename should be set to filename.txt"){
                 REQUIRE(fileName == "filename.txt");
@@ -38,7 +37,7 @@ SCENARIO("testing runParser"){
 
             //works but program crashes if the value can't be converted to string. should be a bug in GenerationsArgument
             THEN("generations should be 3"){
-                REQUIRE(mParser.getAppValues().maxGenerations == 3);
+                REQUIRE(appValues.maxGenerations == 3);
             }
 
             THEN("world dimensions should be 20x10"){
@@ -47,12 +46,91 @@ SCENARIO("testing runParser"){
             }
 
             THEN("even generation should be vonNeumann"){
-                REQUIRE(mParser.getAppValues().evenRuleName == "vonNeumann");
+                REQUIRE(appValues.evenRuleName == "vonNeumann");
             }
 
             THEN("odd generation should be erik"){
-                REQUIRE(mParser.getAppValues().oddRuleName == "erik");
+                REQUIRE(appValues.oddRuleName == "erik");
             }
+        }
+
+        WHEN("we don't specify all rules in argv, runParser() should set correct defaults"){
+
+            WHEN("we only specify the odd rule") {
+                char arg0[] = {"GameOfLife"};
+                char arg1[] = {"-or"};
+                char arg2[] = {"erik"};
+
+
+                char *argv[] = {&arg0[0], &arg1[0], &arg2[0], nullptr};
+
+                ApplicationValues appValues = mParser.runParser(argv, 3);
+
+                THEN("odd generation should be erik") {
+                    REQUIRE(appValues.oddRuleName == "erik");
+                }
+
+                THEN("even rule name should have defaulted to conway") {
+                    REQUIRE(appValues.evenRuleName == "conway");
+                }
+            }
+            WHEN("we only specify the even rule"){
+                char arg0[] = {"GameOfLife"};
+                char arg1[] = {"-er"};
+                char arg2[] = {"erik"};
+
+
+                char *argv[] = {&arg0[0], &arg1[0], &arg2[0], nullptr};
+
+                ApplicationValues appValues = mParser.runParser(argv, 3);
+
+                THEN("odd rule should have been set to the same, ie. erik") {
+                    REQUIRE(appValues.oddRuleName == "erik");
+                }
+
+                THEN("even rule name should be erik") {
+                    REQUIRE(appValues.evenRuleName == "erik");
+                }
+            }
+
+            WHEN("we dont specify any rule names as all"){
+                char arg0[] = {"GameOfLife"};
+
+
+                char *argv[] = {&arg0[0], nullptr};
+
+                ApplicationValues appValues = mParser.runParser(argv, 1);
+
+                THEN("odd rule should not have defaulted to conway") {
+                    REQUIRE(appValues.oddRuleName == "conway");
+                }
+
+                THEN("so should the even rule name") {
+                    REQUIRE(appValues.evenRuleName == "conway");
+                }
+            }
+
+            WHEN("we specify some nonsense options as wekk"){
+
+                char arg0[] = {"GameOfLife"};
+                char arg1[] = {"-xy"};
+                char arg2[] = {"erik"};
+                char arg3[] = {"-f"};
+                char arg4[] = {"minfil.txt"};
+                char arg5[] = {"-jlfkjsdkf"};
+                char arg6[] = {"kul"};
+
+                char *argv[] = {&arg0[0], &arg1[0], &arg2[0], &arg3[0],&arg4[0], &arg5[0], &arg6[0], nullptr};
+
+                ApplicationValues appValues = mParser.runParser(argv, 7);
+
+                THEN("The file value should still be read"){
+                    REQUIRE(fileName == "minfil.txt");
+                }
+
+
+            }
+
         }
 
     }
