97d1bb1c1014b0918343e5a20b10bf22fb5b7002
Fredrik Arhusiander <frar1700@student.miun.se>
2018-10-31 14:26:03 +0100

    comment-gameoflife: commenting done

--- include/GameOfLife.h
+++ include/GameOfLife.h
@@ -1,8 +1,9 @@
-/*
- * Filename    GameOfLife.h
- * Author      Erik Ström
- * Date        October 2017
- * Version     0.1
+/**
+ * @file
+ * @author Erik Ström
+ * @date October 2017
+ * @version 0.1
+ * @brief Contains the class GameOfLife
 */
 
 #ifndef GameOfLifeH
@@ -12,13 +13,14 @@
 #include "ScreenPrinter.h"
 #include <string>
 
-/*
-The heart of the simulation, interconnects the main execution with the graphical presentation.
-
-Creates and manages Population and the ScreenPrinter instances. Will instruct the Population of cells to keep
-updating as long as the specified number of generations is not reached, and for each iteration instruct
-ScreenPrinter to write the information on screen.
-*/
+/**
+ * @class GameOfLife
+ * @brief The heart of the simulation, interconnects the main execution with the graphical presentation.
+ * @details Creates and manages the Population and ScreenPrinter instances. Will instruct the population
+ * of cells to keep updating as long as the specified number of generations is not reached, and for each
+ * iteration instruct ScreenPrinter to print the information to the screen.
+ * @test Constructor and member function
+ */
 class GameOfLife {
 
 private:
@@ -27,8 +29,27 @@ private:
     int nrOfGenerations;
 
 public:
+    /**
+     * @brief Constructor that takes one int and two string values as parameters
+     * @details The int sets the number of generations, the two strings sets the even and odd rule names. An
+     * instance of ScreenPrinter is also created and lastly initiatePopulation() is called on the member
+     * population.
+     * @param nrOfGenerations An int value
+     * @param evenRuleName A string value
+     * @param oddRuleName A string value
+     * @test 1) The even and odd rule names should be correctly set 2) The population should be initiated with
+     * those values.
+     */
     GameOfLife(int nrOfGenerations, string evenRuleName, string oddRuleName);
 
+    /**
+     * @brief Runs the simulation for the set number of generations
+     * @details The default number of generations is 500. The sequence is as follows:
+     * 1. Clear the screen through the ScreenPrinter object
+     * 2. Print the first population onto the screen
+     * 3. Continue to calculate new generations and printing them until all generations have been computed.
+     * @test It should run the correct number of generations
+     */
     void runSimulation();
 
 };
