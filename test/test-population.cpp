907ccfb1f870b822245e5583844a4986ee2ff745
Fredrik Arhusiander <frar1700@student.miun.se>
2018-10-31 11:31:44 +0100

    test-population: fixed comments

--- test/test-population.cpp
+++ test/test-population.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "catch.hpp"
+#include
 #include "Cell_Culture/Population.h"
 #include <typeinfo>
 
@@ -26,48 +27,61 @@ SCENARIO("A population of cells") {
              * Testing calculateNewGeneration()
              */
             THEN("The member generation should be set to 1") {
-                REQUIRE(population.calculateNewGeneration() == 1);
+                int newGen = population.calculateNewGeneration();
+                REQUIRE(newGen == 1);
                 AND_THEN("When we call it again it should be incremented by 1") {
                     REQUIRE(population.calculateNewGeneration() == 2);
                 }
-            }
-            AND_WHEN("We find a Cell that is not alive and has next generation action set to GIVE_CELL_LIFE") {
-                int row = 0;
-                int column = 0;
-                Cell* testCell = nullptr; // create a pointer to a Cell object
-                while(!(row <= WORLD_DIMENSIONS.HEIGHT + 1 | column <= WORLD_DIMENSIONS.WIDTH + 1)) { // stays within world borders
-                    testCell = &population.getCellAtPosition(Point{column, row}); // get reference to a cell
-                    if(!testCell->isRimCell() && !testCell->isAlive() && testCell->getNextGenerationAction() == GIVE_CELL_LIFE)
-                        break; // break if criteria is met
-                    row++;
-                    column++;
-                }
-                /*
-                 * @bug false is returned
-                 */
-                AND_WHEN("We call the function") {
-                    population.calculateNewGeneration();
-                    THEN("The cell should be alive") {
-                        REQUIRE(testCell->isAlive());
+
+                AND_WHEN("We find a Cell that is not alive and has next generation action set to GIVE_CELL_LIFE") {
+                    int row = 0;
+                    int column = 0;
+                    Cell* testCell = nullptr; // create a pointer to a Cell object
+                    int height = WORLD_DIMENSIONS.HEIGHT;
+                    int width = WORLD_DIMENSIONS.WIDTH;
+                    while(row <= height && column <= width) { // stays within world borders
+                        testCell = &population.getCellAtPosition(Point{column, row}); // get reference to a cell
+                        // check so that cell is not rim cell, it's not alive, and the next generation action is set to give cell life
+                        if(!testCell->isRimCell() && testCell->getColor() == STATE_COLORS.DEAD && testCell->getNextGenerationAction() == GIVE_CELL_LIFE) {
+                            break; // break if criteria is met
+                        } else if(column != width)
+                            column++;
+                        else {
+                            column = 0;
+                            row++;
+                        }
+                    }
+
+                    AND_WHEN("We call the function") {
+                        population.calculateNewGeneration();
+                        THEN("The cell should be alive") {
+                            if(testCell == nullptr)
+                                cout << "testCell = nullptr" << endl;
+                            REQUIRE(testCell->getColor() == STATE_COLORS.LIVING);
+                        }
+                    }
+
+                    /*
+                     *  Testing getCellAtPosition()
+                     */
+                    AND_WHEN("We update state and call getCellAtPosition()") {
+                        testCell->updateState();
+                        THEN("We should get a Cell that is alive at the same position") {
+                            REQUIRE(population.getCellAtPosition(Point{column,row}).isAlive());
+                        }
                     }
                 }
 
-                /*
-                 *  Testing getCellAtPosition()
-                 */
-                AND_WHEN("We update state and call getCellAtPosition()") {
-                    testCell->updateState();
-                    THEN("We should get a Cell that is alive at the same position") {
-                        REQUIRE(population.getCellAtPosition(Point{column,row}).isAlive());
-                    }
-                }
             }
+
 
             /*
              * getTotalCellPopulation()
+             *
+             * bug!
              */
             THEN("The cellpopulation should be the same as the world dimensions") {
-                REQUIRE(population.getTotalCellPopulation() == (WORLD_DIMENSIONS.HEIGHT +2) * (WORLD_DIMENSIONS.WIDTH +2) );
+                REQUIRE(population.getTotalCellPopulation() == WORLD_DIMENSIONS.HEIGHT * WORLD_DIMENSIONS.WIDTH);
             }
 
 
@@ -93,7 +107,7 @@ SCENARIO("A population of cells") {
                 for(int j = 0; j < 3; j++) {
                     if(i!=j) { // to only test when the rule names differ
                         population.initiatePopulation(v[i], v[j]);
-                        THEN("There should be a cell population") {
+                        THEN("There should be a cell population with the rules " + v[i] + " and " + v[j]) {
                             REQUIRE(population.getTotalCellPopulation() > 0);
                         }
                     }
